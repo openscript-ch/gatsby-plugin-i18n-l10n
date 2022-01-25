026da1b3e015fae6ee55a49e88cd81e44f884131
Robin <r+git@obin.ch>
2022-01-25 16:38:37 +0000

    ci: enhance target configuration

--- postbuild.js
+++ postbuild.js
@@ -2,3 +2,4 @@ const fs = require('fs');
 
 fs.copyFileSync('./package.json', './dist/package.json');
 fs.copyFileSync('./LICENSE', './dist/LICENSE');
+fs.copyFileSync('./types.d.ts', './dist/types.d.ts');
