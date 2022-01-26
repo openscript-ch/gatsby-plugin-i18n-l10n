4218bead54e257ca675e397028e1b069c3aa0d15
Robin <r+git@obin.ch>
2022-01-26 12:28:29 +0000

    build: publish readme from npm package

--- postbuild.js
+++ postbuild.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
-fs.copyFileSync('./package.json', './dist/package.json');
 fs.copyFileSync('./LICENSE', './dist/LICENSE');
+fs.copyFileSync('./package.json', './dist/package.json');
+fs.copyFileSync('./README.md', './dist/README.md');
 fs.copyFileSync('./types.d.ts', './dist/types.d.ts');
