8dda7e6b7e76d42462f769eb6ccb9fe48389d6ca
Alessandro Maruccia <alesmaru@gmail.com>
2022-06-30 17:01:02 +0200

    fix: avoid printing default language alternate

--- src/components/I18nHead.tsx
+++ src/components/I18nHead.tsx
@@ -15,9 +15,11 @@ export default function I18nHead({ currentLocale, translations, siteUrl, pathnam
       <html lang={currentLocale} />
       <link rel="alternate" hrefLang="x-default" href={siteUrl} />
       {defaultLocale !== currentLocale && <link rel="alternate" hrefLang={currentLocale} href={`${siteUrl}${pathname}`} />}
-      {translations.map((t) => (
-        <link key={t.locale} rel="alternate" hrefLang={t.locale} href={`${siteUrl}${t.path}`} />
-      ))}
+      {translations
+        .filter((t) => t.locale !== `${defaultLocale}`)
+        .map((t) => (
+          <link key={t.locale} rel="alternate" hrefLang={t.locale} href={`${siteUrl}${t.path}`} />
+        ))}
       <meta property="og:locale" content={currentLocale.replace(`-`, `_`)} />
       {translations.map((t) => (
         <meta key={t.locale} property="og:locale:alternate" content={t.locale.replace(`-`, `_`)} />
