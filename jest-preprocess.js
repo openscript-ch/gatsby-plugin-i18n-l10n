d8deffcde71e72ee3973f2506b68ffb35d2b5abd
Robin <git+r@obin.ch>
2022-07-03 14:32:55 +0000

    chore: format jest config files

--- jest-preprocess.js
+++ jest-preprocess.js
@@ -1,8 +1,4 @@
 const babelOptions = {
-  presets: [
-    "babel-preset-gatsby", 
-    "@babel/preset-typescript", 
-    ["@babel/preset-react", { "runtime": "automatic" }]
-  ],
-}
-module.exports = require("babel-jest").default.createTransformer(babelOptions)
+  presets: ['babel-preset-gatsby', '@babel/preset-typescript', ['@babel/preset-react', { runtime: 'automatic' }]],
+};
+module.exports = require('babel-jest').default.createTransformer(babelOptions);
