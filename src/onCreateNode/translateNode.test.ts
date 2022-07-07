2f1bd55644298e10b3cd2b7a106672fa9fa1e381
Robin Bühler <git+r@obin.ch>
2022-07-07 12:11:03 +0000

    feat: resolute translations via nodes

--- src/onCreateNode/translateNode.test.ts
+++ src/onCreateNode/translateNode.test.ts
@@ -1,8 +1,5 @@
-import fs from 'fs/promises';
 import { PluginOptions } from '../../types';
 import { translateNode } from './translateNode';
-
-jest.mock('fs/promises');
 
 const options: PluginOptions = {
   defaultLocale: `en-US`,
@@ -21,8 +18,8 @@ const options: PluginOptions = {
       messages: {},
     },
     {
-      locale: `zh-CN`,
-      prefix: `zh`,
+      locale: `fr-FR`,
+      prefix: `fr`,
       slugs: {},
       messages: {},
     },
@@ -31,185 +28,117 @@ const options: PluginOptions = {
 };
 
 const node = {
-  parent: '1',
+  id: 'm0',
+  parent: '0',
   internal: {
     type: 'MarkdownRemark',
   },
   frontmatter: {
-    title: '',
+    title: 'Impressum',
   },
 };
 
-let createNodeField = jest.fn();
+const allNodes = [
+  {
+    id: '0',
+    internal: {
+      type: 'File',
+    },
+    base: 'imprint.de.md',
+    relativeDirectory: 'pages',
+    dir: '/tmp/project/content/pages',
+    absolutePath: '/tmp/project/content/pages/imprint.de.md',
+    children: ['m0'],
+  },
+  {
+    id: '1',
+    internal: {
+      type: 'File',
+    },
+    base: 'another.de.md',
+    relativeDirectory: 'pages',
+    dir: '/tmp/project/content/pages',
+    absolutePath: '/tmp/project/content/pages/another.de.md',
+  },
+  {
+    id: '2',
+    internal: {
+      type: 'File',
+    },
+    base: 'imprint.en.md',
+    relativeDirectory: 'pages',
+    dir: '/tmp/project/content/pages',
+    absolutePath: '/tmp/project/content/pages/imprint.en.md',
+    children: ['m2'],
+  },
+  {
+    id: '3',
+    internal: {
+      type: 'File',
+    },
+    base: 'imprint.fr.md',
+    relativeDirectory: 'pages',
+    dir: '/tmp/project/content/pages',
+    absolutePath: '/tmp/project/content/pages/imprint.fr.md',
+    children: ['m1'],
+  },
+  {
+    id: '4',
+    internal: {
+      type: 'File',
+    },
+    base: 'more.de.md',
+    relativeDirectory: 'sections/special',
+    dir: '/tmp/project/content/sections',
+    absolutePath: '/tmp/project/content/sections/special/more.de.md',
+  },
+  { ...node },
+  {
+    id: 'm1',
+    parent: '0',
+    internal: {
+      type: 'MarkdownRemark',
+    },
+    frontmatter: {
+      title: 'Imprimer',
+    },
+  },
+  {
+    id: 'm2',
+    parent: '0',
+    internal: {
+      type: 'MarkdownRemark',
+    },
+    frontmatter: {
+      title: 'Imprint',
+    },
+  },
+];
+
+const createNodeField = jest.fn();
+const getNode = jest.fn().mockImplementation((id: string) => allNodes.find((n) => n.id === id));
+const getNodes = jest.fn().mockReturnValue(allNodes);
+const args: any = {
+  getNode,
+  getNodes,
+  node,
+  actions: {
+    createNodeField,
+  },
+};
 
 describe('translateNode', () => {
   beforeEach(() => {
-    createNodeField = jest.fn();
+    jest.clearAllMocks();
   });
 
   it('should create nodes with translation information', async () => {
-    fs.readdir = jest.fn().mockReturnValue(['page.en.md', 'page.zh-CN.md']);
-
-    const parentNode = {
-      base: 'page.de.md',
-      relativeDirectory: 'pages',
-      absolutePath: '/tmp/project/content/pages/page.de.md',
-    };
-
-    const args: any = {
-      getNode: jest.fn().mockReturnValue(parentNode),
-      node,
-      actions: {
-        createNodeField,
-      },
-    };
-
     await translateNode(args, options);
-
-    expect(createNodeField).toHaveBeenNthCalledWith(1, { node, name: 'locale', value: 'de-CH' });
-    expect(createNodeField).toHaveBeenNthCalledWith(2, { node, name: 'filename', value: 'page' });
-    expect(createNodeField).toHaveBeenNthCalledWith(3, { node, name: 'kind', value: 'pages' });
-    expect(createNodeField).toHaveBeenNthCalledWith(4, { node, name: 'slug', value: 'page' });
-    expect(createNodeField).toHaveBeenNthCalledWith(5, { node, name: 'path', value: '/de/pages/page' });
-    expect(createNodeField).toHaveBeenNthCalledWith(6, { node, name: 'pathPrefix', value: 'de' });
-    expect(createNodeField).toHaveBeenNthCalledWith(7, {
-      node,
-      name: 'translations',
-      value: [
-        { locale: 'en-US', path: '/pages/page' },
-        { locale: 'zh-CN', path: '/zh/pages/page' },
-      ],
-    });
-  });
-
-  it('should create nodes with accurate kind description', async () => {
-    fs.readdir = jest.fn().mockReturnValue(['section.en.md', 'section.zh-CN.md']);
-
-    const parentNode = {
-      base: 'section.de.md',
-      relativeDirectory: 'sections/special',
-      absolutePath: '/tmp/project/content/sections/special/section.de.md',
-    };
-
-    const args: any = {
-      getNode: jest.fn().mockReturnValue(parentNode),
-      node,
-      actions: {
-        createNodeField,
-      },
-    };
-
-    await translateNode(args, options);
-
-    expect(createNodeField).toHaveBeenNthCalledWith(1, { node, name: 'locale', value: 'de-CH' });
-    expect(createNodeField).toHaveBeenNthCalledWith(2, { node, name: 'filename', value: 'section' });
-    expect(createNodeField).toHaveBeenNthCalledWith(3, { node, name: 'kind', value: 'sections/special' });
-    expect(createNodeField).toHaveBeenNthCalledWith(4, { node, name: 'slug', value: 'section' });
-    expect(createNodeField).toHaveBeenNthCalledWith(5, { node, name: 'path', value: '/de/sections/special/section' });
-    expect(createNodeField).toHaveBeenNthCalledWith(6, { node, name: 'pathPrefix', value: 'de' });
-    expect(createNodeField).toHaveBeenNthCalledWith(7, {
-      node,
-      name: 'translations',
-      value: [
-        { locale: 'en-US', path: '/sections/special/section' },
-        { locale: 'zh-CN', path: '/zh/sections/special/section' },
-      ],
-    });
-  });
-
-  it('should remote blacklisted path segments when translating nodes', async () => {
-    fs.readdir = jest.fn().mockReturnValue(['page.en.md', 'page.zh-CN.md']);
-
-    const parentNode = {
-      base: 'page.de.md',
-      relativeDirectory: 'pages',
-      absolutePath: '/tmp/project/content/pages/page.de.md',
-    };
-
-    const args: any = {
-      getNode: jest.fn().mockReturnValue(parentNode),
-      node,
-      actions: {
-        createNodeField,
-      },
-    };
-
-    const currentOptions: PluginOptions = {
-      ...options,
-      pathBlacklist: [`/pages`],
-    };
-
-    await translateNode(args, currentOptions);
-
-    expect(createNodeField).toHaveBeenNthCalledWith(1, { node, name: 'locale', value: 'de-CH' });
-    expect(createNodeField).toHaveBeenNthCalledWith(2, { node, name: 'filename', value: 'page' });
-    expect(createNodeField).toHaveBeenNthCalledWith(3, { node, name: 'kind', value: 'pages' });
-    expect(createNodeField).toHaveBeenNthCalledWith(4, { node, name: 'slug', value: 'page' });
-    expect(createNodeField).toHaveBeenNthCalledWith(5, { node, name: 'path', value: '/de/page' });
-    expect(createNodeField).toHaveBeenNthCalledWith(6, { node, name: 'pathPrefix', value: 'de' });
-    expect(createNodeField).toHaveBeenNthCalledWith(7, {
-      node,
-      name: 'translations',
-      value: [
-        { locale: 'en-US', path: '/page' },
-        { locale: 'zh-CN', path: '/zh/page' },
-      ],
-    });
-  });
-
-  it('should translate slugs', async () => {
-    fs.readdir = jest.fn().mockReturnValue(['imprint.en.md', 'imprint.fr.md']);
-
-    const parentNode = {
-      base: 'imprint.de.md',
-      relativeDirectory: 'pages',
-      absolutePath: '/tmp/project/content/pages/imprint.de.md',
-    };
-
-    const args: any = {
-      getNode: jest.fn().mockReturnValue(parentNode),
-      node,
-      actions: {
-        createNodeField,
-      },
-    };
-
-    const currentOptions: PluginOptions = {
-      defaultLocale: `en-US`,
-      siteUrl: '',
-      locales: [
-        {
-          locale: `en-US`,
-          prefix: `en`,
-          slugs: {},
-          messages: {},
-        },
-        {
-          locale: `de-CH`,
-          prefix: `de`,
-          slugs: {
-            '/imprint': '/impressum',
-          },
-          messages: {},
-        },
-        {
-          locale: `fr-FR`,
-          prefix: `fr`,
-          slugs: {
-            '/imprint': '/imprimer',
-          },
-          messages: {},
-        },
-      ],
-      plugins: [],
-    };
-
-    await translateNode(args, currentOptions);
 
     expect(createNodeField).toHaveBeenNthCalledWith(1, { node, name: 'locale', value: 'de-CH' });
     expect(createNodeField).toHaveBeenNthCalledWith(2, { node, name: 'filename', value: 'imprint' });
     expect(createNodeField).toHaveBeenNthCalledWith(3, { node, name: 'kind', value: 'pages' });
-    expect(createNodeField).toHaveBeenNthCalledWith(4, { node, name: 'slug', value: 'imprint' });
+    expect(createNodeField).toHaveBeenNthCalledWith(4, { node, name: 'slug', value: 'impressum' });
     expect(createNodeField).toHaveBeenNthCalledWith(5, { node, name: 'path', value: '/de/pages/impressum' });
     expect(createNodeField).toHaveBeenNthCalledWith(6, { node, name: 'pathPrefix', value: 'de' });
     expect(createNodeField).toHaveBeenNthCalledWith(7, {
@@ -222,99 +151,57 @@ describe('translateNode', () => {
     });
   });
 
-  it('should translate slugs and using the frontmatter titles', async () => {
-    fs.readdir = jest.fn().mockReturnValue(['imprint.de.md', 'imprint.en.md', 'imprint.fr.md']);
-
+  it('should create nodes which are nested in folders', async () => {
     const currentNode = {
-      parent: '0',
+      parent: '4',
       internal: {
         type: 'MarkdownRemark',
       },
-      frontmatter: {
-        title: 'Impressum',
-      },
     };
-
-    const allNodes = [
-      {
-        id: '0',
-        base: 'imprint.de.md',
-        relativeDirectory: 'pages',
-        dir: '/tmp/project/content/pages',
-        absolutePath: '/tmp/project/content/pages/imprint.de.md',
-        children: ['m0'],
-      },
-      {
-        id: '1',
-        base: 'another.de.md',
-        relativeDirectory: 'pages',
-        dir: '/tmp/project/content/pages',
-        absolutePath: '/tmp/project/content/pages/another.de.md',
-      },
-      {
-        id: '2',
-        base: 'imprint.en.md',
-        relativeDirectory: 'pages',
-        dir: '/tmp/project/content/pages',
-        absolutePath: '/tmp/project/content/pages/imprint.en.md',
-        children: ['m2'],
-      },
-      {
-        id: '3',
-        base: 'imprint.fr.md',
-        relativeDirectory: 'pages',
-        dir: '/tmp/project/content/pages',
-        absolutePath: '/tmp/project/content/pages/imprint.fr.md',
-        children: ['m1'],
-      },
-      {
-        id: '4',
-        base: 'more.de.md',
-        relativeDirectory: 'sections',
-        dir: '/tmp/project/content/sections',
-        absolutePath: '/tmp/project/content/sections/more.de.md',
-      },
-      {
-        id: 'm0',
-        parent: '0',
-        internal: {
-          type: 'MarkdownRemark',
-        },
-        frontmatter: {
-          title: 'Impressum',
-        },
-      },
-      {
-        id: 'm1',
-        parent: '0',
-        internal: {
-          type: 'MarkdownRemark',
-        },
-        frontmatter: {
-          title: 'Imprimer',
-        },
-      },
-      {
-        id: 'm2',
-        parent: '0',
-        internal: {
-          type: 'MarkdownRemark',
-        },
-        frontmatter: {
-          title: 'Imprint',
-        },
-      },
-    ];
-
-    const args: any = {
-      getNode: jest.fn().mockImplementation((id: string) => allNodes.find((n) => n.id === id)),
-      getNodes: jest.fn().mockReturnValue(allNodes),
+    const currentArgs = {
+      ...args,
       node: currentNode,
-      actions: {
-        createNodeField,
-      },
+    };
+    await translateNode(currentArgs, options);
+
+    expect(createNodeField).toHaveBeenNthCalledWith(1, { node: currentNode, name: 'locale', value: 'de-CH' });
+    expect(createNodeField).toHaveBeenNthCalledWith(2, { node: currentNode, name: 'filename', value: 'more' });
+    expect(createNodeField).toHaveBeenNthCalledWith(3, { node: currentNode, name: 'kind', value: 'sections/special' });
+    expect(createNodeField).toHaveBeenNthCalledWith(4, { node: currentNode, name: 'slug', value: 'more' });
+    expect(createNodeField).toHaveBeenNthCalledWith(5, { node: currentNode, name: 'path', value: '/de/sections/special/more' });
+    expect(createNodeField).toHaveBeenNthCalledWith(6, { node: currentNode, name: 'pathPrefix', value: 'de' });
+    expect(createNodeField).toHaveBeenNthCalledWith(7, {
+      node: currentNode,
+      name: 'translations',
+      value: [],
+    });
+  });
+
+  it('should remote blacklisted path segments when translating nodes', async () => {
+    const currentOptions: PluginOptions = {
+      ...options,
+      pathBlacklist: [`/pages`],
     };
 
+    await translateNode(args, currentOptions);
+
+    expect(createNodeField).toHaveBeenNthCalledWith(1, { node, name: 'locale', value: 'de-CH' });
+    expect(createNodeField).toHaveBeenNthCalledWith(2, { node, name: 'filename', value: 'imprint' });
+    expect(createNodeField).toHaveBeenNthCalledWith(3, { node, name: 'kind', value: 'pages' });
+    expect(createNodeField).toHaveBeenNthCalledWith(4, { node, name: 'slug', value: 'impressum' });
+    expect(createNodeField).toHaveBeenNthCalledWith(5, { node, name: 'path', value: '/de/impressum' });
+    expect(createNodeField).toHaveBeenNthCalledWith(6, { node, name: 'pathPrefix', value: 'de' });
+    expect(createNodeField).toHaveBeenNthCalledWith(7, {
+      node,
+      name: 'translations',
+      value: [
+        { locale: 'en-US', path: '/imprint' },
+        { locale: 'fr-FR', path: '/fr/imprimer' },
+      ],
+    });
+  });
+
+  it('should translate slugs', async () => {
     const currentOptions: PluginOptions = {
       defaultLocale: `en-US`,
       siteUrl: '',
@@ -328,13 +215,17 @@ describe('translateNode', () => {
         {
           locale: `de-CH`,
           prefix: `de`,
-          slugs: {},
+          slugs: {
+            '/pages': '/seiten',
+          },
           messages: {},
         },
         {
           locale: `fr-FR`,
           prefix: `fr`,
-          slugs: {},
+          slugs: {
+            '/pages': '/feuilles', // I know it's not a literal translation
+          },
           messages: {},
         },
       ],
@@ -343,18 +234,18 @@ describe('translateNode', () => {
 
     await translateNode(args, currentOptions);
 
-    expect(createNodeField).toHaveBeenNthCalledWith(1, { node: currentNode, name: 'locale', value: 'de-CH' });
-    expect(createNodeField).toHaveBeenNthCalledWith(2, { node: currentNode, name: 'filename', value: 'imprint' });
-    expect(createNodeField).toHaveBeenNthCalledWith(3, { node: currentNode, name: 'kind', value: 'pages' });
-    expect(createNodeField).toHaveBeenNthCalledWith(4, { node: currentNode, name: 'slug', value: 'impressum' });
-    expect(createNodeField).toHaveBeenNthCalledWith(5, { node: currentNode, name: 'path', value: '/de/pages/impressum' });
-    expect(createNodeField).toHaveBeenNthCalledWith(6, { node: currentNode, name: 'pathPrefix', value: 'de' });
+    expect(createNodeField).toHaveBeenNthCalledWith(1, { node, name: 'locale', value: 'de-CH' });
+    expect(createNodeField).toHaveBeenNthCalledWith(2, { node, name: 'filename', value: 'imprint' });
+    expect(createNodeField).toHaveBeenNthCalledWith(3, { node, name: 'kind', value: 'pages' });
+    expect(createNodeField).toHaveBeenNthCalledWith(4, { node, name: 'slug', value: 'impressum' });
+    expect(createNodeField).toHaveBeenNthCalledWith(5, { node, name: 'path', value: '/de/seiten/impressum' });
+    expect(createNodeField).toHaveBeenNthCalledWith(6, { node, name: 'pathPrefix', value: 'de' });
     expect(createNodeField).toHaveBeenNthCalledWith(7, {
-      node: currentNode,
+      node,
       name: 'translations',
       value: [
         { locale: 'en-US', path: '/pages/imprint' },
-        { locale: 'fr-FR', path: '/fr/pages/imprimer' },
+        { locale: 'fr-FR', path: '/fr/feuilles/imprimer' },
       ],
     });
   });
--- src/onCreateNode/translateNode.ts
+++ src/onCreateNode/translateNode.ts
@@ -1,7 +1,7 @@
 import { FileSystemNode } from 'gatsby-source-filesystem';
 import convertToSlug from 'limax';
-import fs from 'fs/promises';
 import { posix as path } from 'path';
+import { Node } from 'gatsby';
 import { OnCreateNode, PluginOptions } from '../../types';
 import { addLocalePrefix, replaceSegmentsWithSlugs, trimRightSlash } from '../utils/path';
 import { findClosestLocale, parseFilename } from '../utils/i18n';
@@ -15,25 +15,39 @@ const findLocale = (estimatedLocale: string, options: PluginOptions) => {
   );
 };
 
+const extractFrontmatterTitle = (node?: Node) => {
+  if (node?.frontmatter && typeof node.frontmatter === 'object') {
+    const frontmatter = node.frontmatter as { title?: string };
+    return frontmatter.title;
+  }
+
+  return undefined;
+};
+
 /**
  * Find siblings (files which are translations of the given file)
  *
- * @param absolutePath to the file for which siblings should be searched
+ * @param nodes a list of file system nodes
+ * @param absolutePath of the file which siblings should be searched
  * @param options is the configuration of the current plugin instance
  * @returns siblings paths
  */
-const findTranslations = async (absolutePath: string, options: PluginOptions) => {
-  const fileSiblings = await fs.readdir(path.dirname(absolutePath));
+const findTranslations = async (nodes: Node[], absolutePath: string, options: PluginOptions) => {
+  const fileNodes = nodes.filter((n) => n.internal.type === 'File') as FileSystemNode[];
+  const markdownNodes = nodes.filter((n) => ['MarkdownRemark', 'Mdx'].includes(n.internal.type));
+  const fileSiblings = fileNodes.filter((n) => n.dir === path.dirname(absolutePath));
   const { filename } = parseFilename(path.basename(absolutePath), options.defaultLocale);
   const siblings = fileSiblings
     .filter((f) => {
-      const { filename: siblingFilename } = parseFilename(f, options.defaultLocale);
-      return f !== path.basename(absolutePath) && siblingFilename === filename;
+      const { filename: siblingFilename } = parseFilename(f.base, options.defaultLocale);
+      return f.base !== path.basename(absolutePath) && siblingFilename === filename;
     })
     .map((f) => {
-      const { filename: siblingFilename, estimatedLocale: siblingEstimatedLocale } = parseFilename(f, options.defaultLocale);
+      const { filename: siblingFilename, estimatedLocale: siblingEstimatedLocale } = parseFilename(f.base, options.defaultLocale);
+      const markdownNode = markdownNodes.filter((n) => f.children.map((c) => c).includes(n.id)).find((n) => n);
+      const title = extractFrontmatterTitle(markdownNode);
       const locale = findLocale(siblingEstimatedLocale, options);
-      return { filename: siblingFilename, locale };
+      return { filename: siblingFilename, locale, title };
     });
   return siblings;
 };
@@ -72,19 +86,19 @@ const translatePath = (filename: string, relativeDirectory: string, locale: stri
   return { slug, kind: relativeDirectory, filepath };
 };
 
-export const translateNode: OnCreateNode = async ({ getNode, node, actions }, options) => {
+export const translateNode: OnCreateNode = async ({ getNode, getNodes, node, actions }, options) => {
   const { createNodeField } = actions;
 
   if ((node.internal.type === 'MarkdownRemark' || node.internal.type === 'Mdx') && node.parent && options) {
     const fileSystemNode = getNode(node.parent);
     const { base, relativeDirectory, absolutePath } = fileSystemNode as FileSystemNode;
     const { filename, estimatedLocale } = parseFilename(base, options.defaultLocale);
-    const { title } = node.frontmatter as { title?: string };
+    const title = extractFrontmatterTitle(node);
     const locale = findLocale(estimatedLocale, options);
     const { slug, kind, filepath } = translatePath(filename, relativeDirectory, locale, options, title);
-    const translations = await findTranslations(absolutePath, options);
+    const translations = await findTranslations(getNodes(), absolutePath, options);
     const alternativeLanguagePaths = translations.map((t) => {
-      const { filepath: translatedFilepath } = translatePath(t.filename, relativeDirectory, t.locale, options);
+      const { filepath: translatedFilepath } = translatePath(t.filename, relativeDirectory, t.locale, options, t.title);
       return { path: trimRightSlash(translatedFilepath), locale: t.locale };
     });
     const localeOption = options.locales.find((l) => l.locale === locale);
