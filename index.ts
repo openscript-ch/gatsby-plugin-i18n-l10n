d7fcda9efae0aa30c7f787d08c2e6eeb6616b3f3
Robin Bühler <git+r@obin.ch>
2022-09-22 14:06:25 +0000

    chore: remove i18nhead export

--- index.ts
+++ index.ts
@@ -1,9 +1,7 @@
-import I18nHead from './src/components/I18nHead';
 import LanguageSwitcher from './src/components/LanguageSwitcher';
 import LocalizedLink from './src/components/LocalizedLink';
 import { useI18nL10nContext } from './src/contexts/I18nL10nContext';
 
-export { I18nHead };
 export { LanguageSwitcher };
 export { LocalizedLink };
 export { useI18nL10nContext };
